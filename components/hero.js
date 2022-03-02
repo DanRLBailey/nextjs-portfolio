024d6f1a8a8a4f95efc764bf0f1205e028a27b91
Dan Bailey <danbailey.813@gmail.com>
2022-03-02 20:17:58 +0000

    Added CV & React native link

--- components/hero.js
+++ components/hero.js
@@ -1,21 +1,36 @@
-import styles from '../styles/Hero.module.scss'
-import Image from 'next/image'
+import styles from "../styles/Hero.module.scss";
+import Image from "next/image";
 
 export default function Home() {
-    return (
-        <section className={styles.hero} id='home'>
-            <div className={styles.container}>
-                <div>
-                    <h1>Daniel Bailey</h1>
-                    <h2>Full-Time Software Engineer</h2>
-                    <h2>based in Greater Manchester, U.K.</h2>
-                    <div className={styles.social}>
-                        <div><a href="https://github.com/DanRLBailey" target="_blank"><Image src="/images/github.svg" height={32} width={32} /></a></div>
-                        <div><a href="https://www.linkedin.com/in/daniel-bailey-289979162/" target="_blank"><Image src="/images/linkedin.svg" height={32} width={32} /></a></div>
-                        <div><a href="" target="_blank"><span>CV</span></a></div>
-                    </div>
-                </div>
+  return (
+    <section className={styles.hero} id="home">
+      <div className={styles.container}>
+        <div>
+          <h1>Daniel Bailey</h1>
+          <h2>Full-Time Software Engineer</h2>
+          <h2>based in Greater Manchester, U.K.</h2>
+          <div className={styles.social}>
+            <div>
+              <a href="https://github.com/DanRLBailey" target="_blank">
+                <Image src="/images/github.svg" height={32} width={32} />
+              </a>
             </div>
-        </section>
-    )
+            <div>
+              <a
+                href="https://www.linkedin.com/in/daniel-bailey-289979162/"
+                target="_blank"
+              >
+                <Image src="/images/linkedin.svg" height={32} width={32} />
+              </a>
+            </div>
+            <div>
+              <a href="../CV-PDF.pdf" target="_blank">
+                <span>CV</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
